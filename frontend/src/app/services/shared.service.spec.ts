5bed4153b6f9d1f8a7fe29f0746a70911beb2139
francisco.fernandez <al.francisco.fernandez.vazquez@iesportada.org>
2025-06-04 14:20:45 +0200

    test for all app services

--- frontend/src/app/services/shared.service.spec.ts
+++ frontend/src/app/services/shared.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { SharedListService } from './shared.service';
 
-describe('ServiceService', () => {
+describe('SharedListService', () => {
   let service: SharedListService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [SharedListService]
+    });
     service = TestBed.inject(SharedListService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('debería obtener las listas compartidas', () => {
+    const mockResponse = { data: [{ id: 1, name: 'Lista compartida' }] };
+
+    service.getSharedLists().subscribe((res) => {
+      expect(res.data.length).toBe(1);
+      expect(res.data[0].name).toBe('Lista compartida');
+    });
+
+    const req = httpMock.expectOne('http://localhost:8000/api/shared-lists');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
   });
 });
