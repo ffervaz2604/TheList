9f23551c6ff6c66a0a2a0e465549ccd97ed051f4
francisco.fernandez <al.francisco.fernandez.vazquez@iesportada.org>
2025-06-05 11:04:32 +0200

    changes in the dashboard design

--- frontend/src/app/dashboard/sidebar/sidebar.component.ts
+++ frontend/src/app/dashboard/sidebar/sidebar.component.ts
@@ -2,21 +2,14 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
-import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
+import { TranslocoModule } from '@ngneat/transloco';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule, MatSidenavModule, MatListModule, MatCardModule, TranslocoModule],
+  imports: [RouterModule, MatSidenavModule, MatListModule, TranslocoModule,MatIcon],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
-  constructor(private router: Router) { }
-
-  logout() {
-    localStorage.removeItem('token');
-    this.router.navigate(['/login']);
-  }
-}
+export class SidebarComponent { }
