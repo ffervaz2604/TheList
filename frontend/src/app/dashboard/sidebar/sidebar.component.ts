f129130689a093bea8a5cc2826ddf647cfccc65a
francisco.fernandez <al.francisco.fernandez.vazquez@iesportada.org>
2025-06-06 12:13:53 +0200

    support icon-only sidebar with tooltips

--- frontend/src/app/dashboard/sidebar/sidebar.component.ts
+++ frontend/src/app/dashboard/sidebar/sidebar.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import { TranslocoModule } from '@ngneat/transloco';
 import { MatIcon } from '@angular/material/icon';
+import { TranslocoModule } from '@ngneat/transloco';
+import { CommonModule } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule, MatSidenavModule, MatListModule, TranslocoModule,MatIcon],
+  imports: [
+    RouterModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIcon,
+    TranslocoModule,
+    CommonModule,
+    MatTooltipModule
+  ],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent { }
+export class SidebarComponent {
+  @Input() collapsed = false;
+}
