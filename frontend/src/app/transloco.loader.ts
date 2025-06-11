c3f17b905f92b869eb7b71547047113c11980eb8
francisco.fernandez <al.francisco.fernandez.vazquez@iesportada.org>
2025-06-11 17:28:45 +0200

    fix: set correct baseHref and transloco paths for Apache deployment

--- frontend/src/app/transloco.loader.ts
+++ frontend/src/app/transloco.loader.ts
@@ -2,13 +2,15 @@ import { Translation, TRANSLOCO_LOADER } from '@ngneat/transloco';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { APP_BASE_HREF } from '@angular/common';
+import { Inject } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTranslation(lang: string): Observable<Translation> {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(`/frontend/assets/i18n/${lang}.json`);
   }
 }
 
