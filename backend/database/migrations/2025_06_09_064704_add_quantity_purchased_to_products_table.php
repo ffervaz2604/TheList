71b08317dce0cc84e2f1eb15d580f2c11588d202
francisco.fernandez <al.francisco.fernandez.vazquez@iesportada.org>
2025-06-11 16:46:17 +0200

    feat(api): add quantity_purchased support in products and expose update endpoint

---
+++ backend/database/migrations/2025_06_09_064704_add_quantity_purchased_to_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('quantity_purchased')->default(0)->after('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('quantity_purchased');
+        });
+    }
+};
